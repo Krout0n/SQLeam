3ebb146e4d5342567ee3f5af1c4367cdd1aad1f4
Knium_ <hi.im.knium@gmail.com>
2019-05-22 02:46:47 +0900

    Make REPL more rich.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use statikk_db::ast::AST;
 use statikk_db::parser::Parser;
 use statikk_db::table::Table;
 use statikk_db::tokenizer::Tokenizer;
+use std::io::{self, Write};
 
 fn read() -> String {
     let mut s = String::new();
@@ -13,11 +14,21 @@ fn read() -> String {
 fn main() {
     let mut t = Table::new();
     loop {
+        print!(">> ");
+        io::stdout().flush().unwrap();
+
         let s = read();
+        if &s == "exit\n" {
+            break;
+        }
         let tokens = Tokenizer::new(s.trim()).lex_all();
         let tree = Parser::new(tokens).parse();
         match tree {
-            MethodCall { table: _, name: _, args: _ } => t.command(tree),
+            MethodCall {
+                table: _,
+                name: _,
+                args: _,
+            } => t.command(tree),
             _ => {
                 dbg!(tree);
                 ()
